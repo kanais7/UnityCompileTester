71696e898db6d7628d857c868542e95bb35f714f
kanais7 <kanais0620@gmail.com>
2022-05-02 02:00:16 +0900

    Add Compile Error Script

---
+++ Assets/Scripts/CompileErrorScript.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class CompileErrorScript : MonoBehaviour
+{
+#if UNITY_STANDALONE_OSX
+
+#endif
+
+#if UNITY_IOS
+    aaa
+#endif
+
+#if UNITY_ANDROID
+    aaa
+#endif
+}
